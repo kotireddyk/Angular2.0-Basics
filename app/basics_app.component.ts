05dd6acba6689f18f0d545543a5e21e302301eaf
FGWC <FGWC>
2017-05-18 18:24:45 -0400

    First Commit for Angular2.0 Basics

---
+++ app/basics_app.component.ts
@@ -0,0 +1,13 @@
+import {Component,View} from 'angular2/core';
+
+@Component({
+	selector:'my-app'
+})
+
+@View({
+	template:`<h2>Welcome to Angular2.0</h2>`
+})
+
+export class AppComponent{
+	
+}
---
+++ app/basics_main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from "angular2/platform/browser";
+import {AppComponent} from "./basics_app.component";
+
+bootstrap(AppComponent);
